6e3069878fad3d0bafb2177c89ae60e562cc4893
ShenPaul <shenpaulheng@gmail.com>
2021-07-26 01:12:19 +0200

    adjusted

--- Candycrush_play.cpp
+++ Candycrush_play.cpp
@@ -11,7 +11,7 @@ using namespace std;
 
 HANDLE hConsole;
 
-void game();//遊戲執行 
+void game();//game execution 
 void candy();
 void Erase();
 
@@ -34,47 +34,47 @@ int main()
     char key , site=1;
 
 	while(1)
-	{  system("cls");//清除畫面
+	{  system("cls");//clean screen
         
 		if(site==1)
-		{   cout<<setw(4)<<"*"<<setw(2)<<"開始遊戲"<<endl;
-			cout<<setw(4)<<" "<<setw(2)<<"規則說明"<<endl;
-			cout<<setw(4)<<" "<<setw(2)<<"離開遊戲"<<endl;}
+		{   cout<<setw(4)<<"*"<<setw(2)<<"start game"<<endl;
+			cout<<setw(4)<<" "<<setw(2)<<"Rule"<<endl;
+			cout<<setw(4)<<" "<<setw(2)<<"Exit"<<endl;}
 		if(site==2)
-		{	cout<<setw(4)<<" "<<setw(2)<<"開始遊戲"<<endl;
-			cout<<setw(4)<<"*"<<setw(2)<<"規則說明"<<endl;
-			cout<<setw(4)<<" "<<setw(2)<<"離開遊戲"<<endl;}
+		{   cout<<setw(4)<<""<<setw(2)<<"start game"<<endl;
+			cout<<setw(4)<<"*"<<setw(2)<<"Rule"<<endl;
+			cout<<setw(4)<<" "<<setw(2)<<"Exit"<<endl;}
 		if(site==3)
-		{	cout<<setw(4)<<" "<<setw(2)<<"開始遊戲"<<endl;
-			cout<<setw(4)<<" "<<setw(2)<<"規則說明"<<endl;
-			cout<<setw(4)<<"*"<<setw(2)<<"離開遊戲"<<endl;}//遊戲開始畫面 按上下鍵時 決定*的位置
+		{   cout<<setw(4)<<""<<setw(2)<<"start game"<<endl;
+			cout<<setw(4)<<" "<<setw(2)<<"Rule"<<endl;
+			cout<<setw(4)<<"*"<<setw(2)<<"Exit"<<endl;}//use up and down to decide the position *
 		if(key=getch()==13)//13=Enter鍵
 		{	
 		    if(site==1)
 		    game();
 		
-		    if(site==2)//遊戲規則
-			{	system("cls");//清除畫面
-				cout<<" 這是一個10x10大小的矩形糖果盤，移動到欲交換的糖果，按C鍵為選取"<<endl<<endl;
-				cout<<" 被選取的相鄰兩個糖果座標即可交換糖果 "<<endl<<endl;
-				cout<<" 當三個糖果相連，糖果會消除並由上往下填補"<<endl<<endl;
-				cout<<" 消除3個 □ 或 ☆ 得五分；消除3個 ○ 或 ◇ 得十分"<<endl<<endl;
-				cout<<" 每移動一次糖果步數減少一次，當移動到0次遊戲即結束"<<endl<<endl;
-				cout<<" 請輸入ENTER以返回前頁"<<endl<<endl;
-				while(getch()!=13);//輸入enter返回
+		    if(site==2)//Rule
+			{	system("cls");//Clean
+				cout<<" This is a 10x10 rectangular candy tray. Move to the candy you want to exchange and press C to select"<<endl<<endl;
+				cout<<" Two adjacent candy coordinates can be selected to exchange candy "<<endl<<endl;
+				cout<<" When three candies are connected, the candies will be eliminated and filled from top to bottom"<<endl<<endl;
+				cout<<" Eliminate 3 □ or ☆ to get 5 points; eliminate 3 ○ or ◇ to get 10"<<endl<<endl;
+				cout<<" Each time you move the candy steps, the number of steps will decrease once, and the game will end when you move to 0 times"<<endl<<endl;
+				cout<<" Please enter ENTER to return to the previous page"<<endl<<endl;
+				while(getch()!=13);//input Enter to go back
 			}
-			if(site==3)//離開遊戲
+			if(site==3)//leave game
 			exit(0);
 		 }
 		else
 		{ switch(key=getch())
 			{
-				case 'w': // Up鍵
+				case 'w': // Up
 				case 'W':
 					site--;
 					break;
 				case 'S':
-				case 's': // Down鍵
+				case 's': // Down
 					site++;
 					break;}
 			if(site>3)
@@ -90,7 +90,7 @@ int main()
 }
 
 
-void game()//遊戲執行 
+void game()//Game execution
 {
 	
 	char array2[10]={'A','B','C','D','E','F','G','H','I','J'};
@@ -100,7 +100,7 @@ void game()//遊戲執行
 	srand( time(0) );
 	
 	cout<<"   ";
-	for(int w = 0;w < 10; w++ )  //英文字母 
+	for(int w = 0;w < 10; w++ )  
 	{   setColor(3);
 		cout << setw(2) << array2[w];  
 	}
@@ -112,15 +112,15 @@ void game()//遊戲執行
 	
 	for(int i = 0; i < 10; i++)
 	{
-		for(int j = 0;j < 10; j++) //設定陣列的變數 
+		for(int j = 0;j < 10; j++) 
 		{    
 			array[i][j] = rand() % 4;
 		}
 	}
-	//起始反白 
+	
 		
 		O=array[X][Y];
-		//重新指定(為了反白) 
+	
 		if(array[X][Y]==0)
 		array[X][Y]=4;
 		
@@ -136,12 +136,12 @@ void game()//遊戲執行
 
 	///////////////////////////
 	
-	for(int k = 0;k < 10 ;k++) //糖果列 
+	for(int k = 0;k < 10 ;k++) //Candy row
 	{
 		setColor(3);
 		cout<<setw(2)<<k+1<<" ";
 		 
-		for(int q = 0;q < 10 ;q++) //列的column 
+		for(int q = 0;q < 10 ;q++) //column 
 		{
 	    	if( array[k][q] == 0)
 	    	{
@@ -185,8 +185,8 @@ void game()//遊戲執行
 		cout << endl;
 	}
 array[X][Y]=O;
-/////////////////////分隔線///////////////////////////////////////////////////////////////////////////////////////////
-int counter=15;//計數 
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+int counter=15;// 
 
 
 
@@ -198,12 +198,12 @@ candy();
 cout<<endl;
 
 setColor(7);
-cout << "消除3個 □ 或 ☆ 得五分" << endl << "消除3個 ○ 或 ◇ 得十分" << endl << endl; 
-cout<<"剩餘步數："<< counter <<setw(10)<<"分數:"<< score << endl<<endl;
+cout << "Clear 3 □ or ☆ 5 points" << endl << "Eliminate 3 ○ or ◇ to get 10 points" << endl << endl; 
+cout<<"Remaining steps:"<< counter <<setw(10)<<"Score:"<< score << endl<<endl;
 
 while(counter > 0)
 {
-//交換
+//swap
 	while(C1==-1 || C2==-1)
 	{
 		char presskey=getch(); 
@@ -247,7 +247,7 @@ while(counter > 0)
 		system("cls");
 		
 		O=array[X][Y];
-		//重新指定(為了反白) 
+		
 		if(array[X][Y]==0)
 		array[X][Y]=4;
 		
@@ -274,8 +274,8 @@ while(counter > 0)
 cout<<endl;
 
 setColor(7);
-cout << "消除3個 □ 或 ☆ 得五分" << endl << "消除3個 ○ 或 ◇ 得十分" << endl << endl; 
-cout<<"剩餘步數："<< counter <<setw(10)<<"分數:"<< score << endl<<endl;
+cout << "Clear 3 □ or ☆ 5 points" << endl << "Eliminate 3 ○ or ◇ to get 10 points" << endl << endl; 
+cout<<"Remaining steps:"<< counter <<setw(10)<<"Score:"<< score << endl<<endl;
 	
 	if(mid==0)
 		array[X][Y]=O;
@@ -293,7 +293,7 @@ cout<<"剩餘步數："<< counter <<setw(10)<<"分數:"<< score << endl<<endl;
  C1=-1,C2=-1;
 
 
-//////////////////////////////////////////////////////////////////////////////////// 確保都依經消完 
+//////////////////////////////////////////////////////////////////////////////////// 
 for(int U=0;U<30;U++)
 Erase(); 
 candy();
@@ -306,8 +306,8 @@ counter--;
 cout<<endl;
 
 setColor(7);
-cout << "消除3個 □ 或 ☆ 得五分" << endl << "消除3個 ○ 或 ◇ 得十分" << endl << endl; 
-cout<<"剩餘步數："<< counter <<setw(10)<<"分數:"<< score << endl<<endl;
+cout << "Clear 3 □ or ☆ 5 points" << endl << "Eliminate 3 ○ or ◇ to get 10 points" << endl << endl; 
+cout<<"Remaining steps:"<< counter <<setw(10)<<"Score:"<< score << endl<<endl;
 
 
 }
@@ -318,8 +318,8 @@ char Ans;
 
 setColor(7);
 cout<<endl;
-cout<<"總得分；"<<score; 
-cout<<"要再玩一次嗎？(Y or N)";
+cout<<"Total Score"<<score; 
+cout<<"Play it again?(y or n)";
 cout<<endl;
 cin>>Ans;
 switch(Ans)
@@ -336,22 +336,22 @@ switch(Ans)
 }
 }
 
-void candy()//印candy
+void candy()//print game
 {  
    
 	cout<<"   ";
-	for(int w = 0;w < 10; w++ )  //英文字母 
+	for(int w = 0;w < 10; w++ )  
 	{   setColor(3);
 		cout << setw(2) << array2[w];  
 	}
 	
 	cout << endl;
-	for(int k = 0;k < 10 ;k++) //糖果列 
+	for(int k = 0;k < 10 ;k++) 
 	{
 		setColor(3);
 		cout<<setw(2)<<k+1<<" ";
 		 
-		for(int q = 0;q < 10 ;q++) //列的column 
+		for(int q = 0;q < 10 ;q++) 
 		{
 	    	if( array[k][q] == 0)
 	    	{
@@ -397,15 +397,15 @@ void candy()//印candy
 		
 }
 
-void Erase()//消糖果 
+void Erase()//Erase Candy 
 {
-	for(int l=0;l<10;l++)//消糖果 (橫的) 
+	for(int l=0;l<10;l++)
 {
 	for(int m=1;m<10;m++)
 	{
 		if(array[l][m-1]==array[l][m] && array[l][m]==array[l][m+1])
 	    {  
-	       switch(array[l][m])//消除糖果得分 
+	       switch(array[l][m])
 			{
 				case 0:
 					score += 5;
@@ -443,13 +443,13 @@ void Erase()//消糖果
  } 
  int J=0;
  
- for(int n=0;n<10;n++)//消糖果 (直的) 
+ for(int n=0;n<10;n++)
 {
 	for(int o=1;o<10;o++)
 	{
 		if(array[o-1][n]==array[o][n] && array[o][n]==array[o+1][n])
 	    {
-	         switch(array[o][n])//消除糖果得分
+	         switch(array[o][n])
 			{
 				case 0:
 					score += 5;
@@ -469,7 +469,7 @@ void Erase()//消糖果
 			 
 			 J=o+1;
 			 
-			      if(J>=5)//消三補三
+			      if(J>=5)
 			         {
 			  
 				        while(J>4 && J<10) 
@@ -485,7 +485,7 @@ void Erase()//消糖果
 			            array[2][n]=rand() % 4;
 			        }
 			    
-			      else if(J==4) //消三補二 
+			      else if(J==4) 
 	                  {
 	                  	
 		                array[J-1][n]=array[J-4][n]; //3-0
@@ -495,7 +495,7 @@ void Erase()//消糖果
 			            array[1][n]=rand() % 4;
 			            array[2][n]=rand() % 4;
 					  }
-				 else if(J==3) //消三補一 
+				 else if(J==3) 
 	                  {
 	                  	
 		                array[J][n]=array[J-3][n]; //3-0 
@@ -504,7 +504,7 @@ void Erase()//消糖果
 			            array[1][n]=rand() % 4;
 			            array[2][n]=rand() % 4;
 					  } 
-				else if(J==2) //消三補零 
+				else if(J==2)
 	                  {
 	                  	
 		                             	
